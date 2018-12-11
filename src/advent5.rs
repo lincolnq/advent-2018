a8c6160a54333b122a0f94f58dee0e66d4dadccc
Lincoln Quirk <lincolnq@gmail.com>
2018-12-11 00:55:48 +0000

    a much faster implementation

--- src/advent5.rs
+++ src/advent5.rs
@@ -1,43 +1,68 @@
 use itertools::Itertools;
 use itertools::free::enumerate;
+use std::collections::BTreeMap;
+use std::ops::Neg;
 
-pub fn reacts(c1: char, c2: char) -> bool {
-    return c1.to_uppercase().to_string() == c2.to_uppercase().to_string() &&
-        ((c1.is_lowercase() && c2.is_uppercase())
-            || (c1.is_uppercase() && c2.is_lowercase()))
+type Polymer = BTreeMap<i32, i8>;
+
+pub fn reacts(c1: i8, c2: i8) -> bool {
+    c1 == -c2
 }
 
-pub fn find_reaction(s: &String) -> Option<usize> {
-    let windows = s.chars().tuple_windows::<(char, char)>();
-    for (ix, (l, r)) in enumerate(windows) {
-        if reacts(l,r) {
-            return Some(ix)
+pub fn find_reactions(s: &Polymer) -> Vec<(i32, i32)> {
+    let windows = s.iter().tuple_windows::<((&i32, &i8), (&i32, &i8))>();
+    let mut result = Vec::new();
+    let mut last_key = -1;
+    for (l, r) in windows {
+        if *l.0 == last_key {
+            continue;
+        }
+        if reacts(*l.1,*r.1) {
+            result.push((*l.0, *r.0));
+            // don't react again
+            last_key = *r.0
         }
     }
-    None
+    result
 }
 
-pub fn step(s: String) -> String {
-    if let Some(ix) = find_reaction(&s) {
-        // react
-        let mut res = String::from(&s[..ix]);
-        res.push_str(&s[ix+2..]);
-        res
-    } else {
-        s
+pub fn step(s: &mut Polymer) {
+    let reactions = find_reactions(s);
+    for r in reactions {
+        s.remove(&r.0);
+        s.remove(&r.1);
     }
+}
+
+fn to_num(c: u8) -> i8 {
+    let ci = c as i8;
+    let upper_base = 'A' as i8 - 1;
+    let lower_base = 'a' as i8 - 1;
+    if ci >= lower_base {
+        -(ci - lower_base)
+    } else {
+        ci - upper_base
+    }
+}
+
+fn parse(s: String) -> Polymer {
+    let mut map = BTreeMap::new();
+    map.extend(enumerate(s.trim().bytes().map(to_num)).map(|(i, c)| (i as i32, c)));
+    map
 }
 
 pub fn advent5(s: String) -> Result<i32, &'static str> {
     println!("start: {}", &s);
-    let mut result = String::from(s.trim());
-    let mut length = result.len();
+    let mut map = parse(s);
+    println!("map: {:?}", &map);
+
+    let mut length = map.len();
     loop {
-        result = step(result);
-        if result.len() == length {
+        step(&mut map);
+        if map.len() == length {
             break
         }
-        length = result.len();
+        length = map.len();
         if length % 100 == 0 {
             println!("len: {}", length);
         }
